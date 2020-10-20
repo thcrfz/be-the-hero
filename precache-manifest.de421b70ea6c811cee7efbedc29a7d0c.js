6da8de3d855f1fb8d25d00ad678fcf4ebc82cc67
Thiago Cruz <thiago2096@live.com>
2020-10-19 22:15:01 -0400

    Updates

---
+++ precache-manifest.de421b70ea6c811cee7efbedc29a7d0c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7487bcd441fbcc6db3e52c5f821ba769",
+    "url": "/be-the-hero/index.html"
+  },
+  {
+    "revision": "63d770b9c017b871593d",
+    "url": "/be-the-hero/static/css/main.1c5b4f87.chunk.css"
+  },
+  {
+    "revision": "4d4d1af3e63212ace8cc",
+    "url": "/be-the-hero/static/js/2.0a0cf3c5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/be-the-hero/static/js/2.0a0cf3c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63d770b9c017b871593d",
+    "url": "/be-the-hero/static/js/main.81b80ba2.chunk.js"
+  },
+  {
+    "revision": "9f3cf39f63b98f354727",
+    "url": "/be-the-hero/static/js/runtime-main.f5e4c981.js"
+  },
+  {
+    "revision": "1f4b5508873e87c04ab679e647a7954b",
+    "url": "/be-the-hero/static/media/heroes.1f4b5508.png"
+  },
+  {
+    "revision": "7eea718b48893e461f82049ec46c1e1c",
+    "url": "/be-the-hero/static/media/logo.7eea718b.svg"
+  }
+]);
